22d81cb618026824b302d09ad5c839924b952c2f
ajoy biswas <ajoypust@gmail.comm>
2022-06-29 17:42:44 +0600

    heroku add

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express=require('express');
 const cors = require('cors');
+var jwt = require('jsonwebtoken');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port=process.env.PORT||5000;
 require('dotenv').config()
@@ -17,6 +18,7 @@ async function run(){
   try{
       await client.connect();
       const serviceCollection=client.db("CarHat").collection("services");
+      const orderCollection=client.db("car_Hat").collection("order");
       // data reading/load
       app.get('/service',async(req,res)=>{
         const query={};
@@ -45,6 +47,32 @@ async function run(){
         res.send(result)
 
       })
+
+      // create a place oredr
+      app.post('/order',async(req,res)=>{
+        const order=req.body;
+        const result=await orderCollection.insertOne(order);
+        console.log(result)
+        res.send(result)
+      });
+
+      //get data by order page
+      app.get('/order',async(req,res)=>{
+        const email=req.query.email;
+        const query={email};
+        const orders=orderCollection.find(query);
+        const result=await orders.toArray();
+        res.send(result)
+      })
+      // jwt api create
+      app.post('/login',async(req,res)=>{
+        const user=req.body;
+        const accessToken=jwt.sign(user,process.env.ACCESS_TOKEN,{
+          expiresIn:'5h'
+        });
+        res.send({accessToken})
+      })
+
   }
   finally{
 
